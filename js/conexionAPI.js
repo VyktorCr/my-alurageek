24be20b3262cdbb4a54ca083c3499874c42c880c
Vyktor Cr <hawk5056@protonmail.com>
2024-06-05 20:56:52 -0700

    node datos

--- js/conexionAPI.js
+++ js/conexionAPI.js
@@ -7,7 +7,24 @@ async function listaProductos(){
     return conexionConvertida
 
 }
+
+async function crearProducto(id,image,precio,trash){
+    const conexion = await fetch ("http://localhost:3001/productos",{
+        method:"POST",
+        headers:{"Content-type":"application/json"},
+        body:JSON.stringify({
+            id:id,
+            image:image,
+            precio:precio,
+            trash:trash
+        })
+    })
+    const conexionConvertida= conexion.json();
+
+    return conexionConvertida;
+}
+
 export const conexionAPI={
-    listaProductos
+    listaProductos,crearProducto
 }
 //listaProductos()
